1d350fc3b2168f663a78c4f06127c51ec29abf53
yunzheyue123 <1301002716@qq.com>
2019-01-14 11:37:12 +0800

    seventh

--- app/src/main/java/com/example/administrator/project_sevent/Main2Activity.java
+++ app/src/main/java/com/example/administrator/project_sevent/Main2Activity.java
@@ -23,6 +23,7 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Log.e("TAG", "master");
+        Log.e("TAG", "master1");
         Log.e("TAG", "branch1");
 
         new Thread(){
