0f1f3d64a8a3d31b774eb9ee41e35676165945a8
yunzheyue123 <1301002716@qq.com>
2019-01-14 11:14:47 +0800

    fourth

--- app/src/main/java/com/example/administrator/project_sevent/Main2Activity.java
+++ app/src/main/java/com/example/administrator/project_sevent/Main2Activity.java
@@ -22,7 +22,7 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Log.e("TAG", "branch");
+        Log.e("TAG", "branch1");
 
         new Thread(){
             @Override
